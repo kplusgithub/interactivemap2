bfcfc112bcd192371528b8d54fb44e7dccaa2fb2
KPLUSKONZEPT\t.geiger <geiger@kplus-konzept.de>
2017-06-12 15:57:05 +0200

    11 commit

--- js/interactive.js
+++ js/interactive.js
@@ -25,26 +25,6 @@ $(document).ready(function () { "use strict";
      });
 
 
-/* SIDENAV */
-
-
-
-var $body   = $(document.body);
-var navHeight = $('.navbar').outerHeight(true) + 10;
-
-$('#sidebar').affix({
-    offset: {
-        top: 245,
-        bottom: navHeight
-    }
-});
-
-
-$body.scrollspy({
-    target: '#leftCol',
-    offset: navHeight
-});
-
 
 
 
