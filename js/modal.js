5a3f243d6882f7e1408ad88a2e106cd4c2630b1a
KPLUSKONZEPT\t.geiger <geiger@kplus-konzept.de>
2017-10-02 15:29:30 +0200

    50-commit

--- js/modal.js
+++ js/modal.js
@@ -25,3 +25,21 @@ $('[id="hotspot01"]').click(function()
     $("#hotspot01modal").modal();
 
 });
+
+$('[id="hotspot02"]').click(function()
+{
+    $("#hotspot02modal").modal();
+
+});
+
+$('[id="hotspot03"]').click(function()
+{
+    $("#hotspot03modal").modal();
+
+});
+
+$('[id="hotspot04"]').click(function()
+{
+    $("#hotspot04modal").modal();
+
+});
